2db43fd7e050847a336a116659576e17199e9b2c
jisoo <doh_0112@naver.com>
2023-04-20 21:00:16 +0900

    working 스타일 컴포넌트 생성

--- src/componenets/Working.jsx
+++ src/componenets/Working.jsx
@@ -1,19 +1,40 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import ItemTodo from "./ItemTodo";
+import styled from "styled-components";
+
+// styled components
+const StBox = styled.div`
+  text-align: left;
+  padding: 0 100px 0 100px;
+`;
+const StTitle = styled.h2`
+  font-size: 26px;
+  font-weight: 700;
+  padding: 30px 0;
+`;
+
+const StBoxItems = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 300px);
+  justify-content: space-around;
+  gap: 40px 0;
+`;
 
 const Working = () => {
   const dataStore = useSelector((state) => state.setData);
 
   return (
-    <div>
-      <h2>Working</h2>
-      {dataStore
-        .filter((todo) => !todo.isDone)
-        .map((todo) => (
-          <ItemTodo todo={todo} key={todo.id} />
-        ))}
-    </div>
+    <StBox>
+      <StTitle>Working</StTitle>
+      <StBoxItems>
+        {dataStore
+          .filter((todo) => !todo.isDone)
+          .map((todo) => (
+            <ItemTodo todo={todo} key={todo.id} />
+          ))}
+      </StBoxItems>
+    </StBox>
   );
 };
 
