dcf566854417370112a335d807f89ea6e34a49bb
jisoo <doh_0112@naver.com>
2023-04-20 21:00:55 +0900

    home 스타일 컴포넌트 생성

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,18 @@
 import CreateTodo from "../componenets/CreateTodo";
 import Working from "../componenets/Working";
 import Done from "../componenets/Done";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  padding: 15px 50px;
+  font-size: 30px;
+  font-weight: 800;
+`;
 
 function Home() {
   return (
     <>
-      <h1>MY TO-DO</h1>
+      <Title>MY TO-DO</Title>
       <CreateTodo />
       <Working />
       <Done />
