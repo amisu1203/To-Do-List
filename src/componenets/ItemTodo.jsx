ffe43a62ee23a589aa778f3ac8e58dc01b7a9171
jisoo <doh_0112@naver.com>
2023-04-20 21:00:01 +0900

    ItemTodo 스타일 컴포넌트 생성

--- src/componenets/ItemTodo.jsx
+++ src/componenets/ItemTodo.jsx
@@ -3,7 +3,62 @@ import { useDispatch } from "react-redux";
 import { remove_todo, get_done, turn_back } from "../redux/modules/setData";
 import { useNavigate } from "react-router-dom";
 import { clearTodo } from "../redux/modules/setTodo";
-// import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+// styled componenets
+const StBox = styled.div`
+  background-color: #c7cabd;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  border-radius: 10px;
+  background-color: #d9e4f5;
+  background-image: linear-gradient(315deg, #d9e4f5 0%, #f5e3e6 74%);
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  padding: 20px 0;
+`;
+
+const BtnGoDetail = styled.button`
+  border: none;
+  line-height: 30px;
+  padding: 0 20px;
+  font-size: 15px;
+  background-color: transparent;
+  transition: all 0.2s ease-in;
+  cursor: pointer;
+`;
+
+const StTitle = styled.h1`
+  font-size: 24px;
+  font-weight: 600;
+  margin: 10px 0;
+`;
+
+const StTxt = styled.p`
+  font-size: 18px;
+  margin-bottom: 10px;
+`;
+
+const StBoxBtns = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+`;
+
+const StBtn = styled.button`
+  border: none;
+  border-radius: 10px;
+  line-height: 30px;
+  padding: 0 19px;
+  font-size: 17px;
+  background-color: rgb(255, 255, 255);
+  transition: all 0.2s ease-in;
+  &:hover {
+    scale: 1.1;
+  }
+`;
 
 function ItemTodo({ todo }) {
   const dispatch = useDispatch();
@@ -34,36 +89,37 @@ function ItemTodo({ todo }) {
     alert("다시 할 일이 추가됐네요!");
   };
   return todo.isDone ? (
-    <>
+    <StBox>
+      <BtnGoDetail onClick={goDetail}>상세 보기</BtnGoDetail>
       <article className="DoneItem box-same1">
-        <h2>{todo.title}</h2>
-        <p>{todo.content}</p>
-        <div className="con-btns">
-          <button className="btn-delete btn-original" onClick={handleRemove}>
+        <StTitle>{todo.title}</StTitle>
+        <StTxt>{todo.content}</StTxt>
+        <StBoxBtns className="con-btns">
+          <StBtn className="btn-delete btn-original" onClick={handleRemove}>
             삭제
-          </button>
-          <button className="btn-original" onClick={handleTurnBack}>
+          </StBtn>
+          <StBtn className="btn-original" onClick={handleTurnBack}>
             취소
-          </button>
-        </div>
+          </StBtn>
+        </StBoxBtns>
       </article>
-    </>
+    </StBox>
   ) : (
-    <>
-      <button onClick={goDetail}>상세 페이지로 가기</button>
+    <StBox>
+      <BtnGoDetail onClick={goDetail}>상세 보기</BtnGoDetail>
       <article className="TodoItem box-same1">
-        <h2>{todo.title}</h2>
-        <p>{todo.content}</p>
-        <div className="con-btns">
-          <button className="btn-delete btn-original" onClick={handleRemove}>
+        <StTitle>{todo.title}</StTitle>
+        <StTxt>{todo.content}</StTxt>
+        <StBoxBtns className="con-btns">
+          <StBtn className="btn-delete btn-original" onClick={handleRemove}>
             삭제
-          </button>
-          <button className="btn-original" onClick={handleDone}>
+          </StBtn>
+          <StBtn className="btn-original" onClick={handleDone}>
             완료
-          </button>
-        </div>
+          </StBtn>
+        </StBoxBtns>
       </article>
-    </>
+    </StBox>
   );
 }
 
