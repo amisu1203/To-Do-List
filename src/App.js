9fa1a59f322e49503f02f74b14a4e92fca6feca8
jisoo <doh_0112@naver.com>
2023-04-20 21:01:09 +0900

    App 스타일 컴포넌트 생성

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import "./App.css";
+import "./reset.css";
 import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import DetailTodo from "./pages/DetailTodo";
 import Home from "./pages/home";
+import styled from "styled-components";
+
+const StBox = styled.div`
+  // 그리고 이 안에 스타일 코드를 작성합니다. 스타일 코드는 우리가 알고 있는 css와 동일합니다.
+  max-width: 1200px;
+  margin: 0 auto;
+  text-align: center;
+`;
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <StBox>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/detail/:id" element={<DetailTodo />} />
         </Routes>
-      </div>
+      </StBox>
     </BrowserRouter>
   );
 }
